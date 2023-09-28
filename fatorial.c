12a48f3eaa4780021de46b9e4c64b499b38fe83d
Heloisa Feitosa <103234249+feitosa16@users.noreply.github.com>
2023-09-28 22:51:47 +0000

    my second commit

--- fatorial.c
+++ fatorial.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-int n, fatorial;
+int n;
+int fat = 1;
 
 scanf("%d",&n);
-    for (int i = 0 ; i < n; i--){
-      fatorial = fatorial *n;
+    for (fat = fat; n >1; n = n -1){
+      fat = fat * n; 
     }
-printf("%d",fatorial);
-8
+    printf("O valor do fatorial é igual a %d\n", fat);
     return 0;
 }
