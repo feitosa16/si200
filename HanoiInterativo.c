48d92215f5a57d40dd2b9a9e4008b40776931ce4
Heloisa Feitosa <103234249+feitosa16@users.noreply.github.com>
2023-12-12 22:47:26 +0000

    my third commit

--- HanoiInterativo.c
+++ HanoiInterativo.c
@@ -3,27 +3,35 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+// Função recursiva para resolver a Torre de Hanoi
 void hanoi(int n, char origem, char destino, char auxiliar) {
     if (n == 1) {
+        // Se há apenas um disco, mova-o diretamente para o destino
         printf("\nMova o disco 1 da pilha %c para a pilha %c", origem, destino);
         return;
     }
-    
+
+    // Mover n-1 discos de origem para auxiliar usando destino como auxiliar
     hanoi(n-1, origem, auxiliar, destino);
-    
+
+    // Mover o disco restante de origem para destino
     printf("\nMova o disco %d da pilha %c para a pilha %c", n, origem, destino);
-    
+
+    // Mover n-1 discos de auxiliar para destino usando origem como auxiliar
     hanoi(n-1, auxiliar, destino, origem);
 }
 
 int main() {
+    // Solicitar ao usuário o número de discos
     char *input = readline("Digite o numero de discos: ");
-    int n = atoi(input);
-    free(input);
+    int n = atoi(input);  // Converter a entrada de string para inteiro
+    free(input);  // Liberar a memória alocada para a string de entrada
 
+    // Chamar a função de Hanoi para resolver o problema
     hanoi(n, 'A', 'C', 'B');
-    
+
+    // Exibir uma nova linha no final para melhor formatação
     printf("\n");
-    
+
     return 0;
 }
